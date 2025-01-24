827e6c3aa742eca3d4b0b390e5cd1b992ced63ee
Koushik15622 <Koushikpln02@gmail.com>
2025-01-23 20:51:49 -0500

    bug: server change

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://wc-assignment.vercel.app/",
+  baseURL: "https://5b7e-38-27-127-37.ngrok-free.app",
   withCredentials: true,
 });
 
