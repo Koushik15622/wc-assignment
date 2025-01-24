67f61ddd9161ffc11b75845aa7582a900a6ea134
Koushik15622 <Koushikpln02@gmail.com>
2025-01-23 21:01:09 -0500

    minor fix

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://wc-assignment.vercel.app/",
+  baseURL: "https://wc-assignment.vercel.app",
   withCredentials: true,
 });
 
